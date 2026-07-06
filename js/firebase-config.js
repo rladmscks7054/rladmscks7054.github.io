5f7e15a20bc47048f3e99f3948dcd983e86f9aa0
rladmscks7054 <rladmscks7054@naver.com>
2026-07-06 11:54:05 +0900

    Add Firebase configuration file
    Added Firebase configuration for the project.

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAi0yoqEuwwui6lItbS1g6uUShEPX5Phas",
+  authDomain: "truthandpraisechurch.firebaseapp.com",
+  projectId: "truthandpraisechurch",
+  storageBucket: "truthandpraisechurch.firebasestorage.app",
+  messagingSenderId: "764723618419",
+  appId: "1:764723618419:web:3b83c218904a067656c006",
+  measurementId: "G-8H8T7560LL"
+};
