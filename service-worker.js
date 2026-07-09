768f9a291304ada7496bcf8faa8e0126cec1d704
rladmscks7054 <rladmscks7054@naver.com>
2026-07-09 17:26:30 +0900

    Implement service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME="church-v1";
+
+const FILES=[
+
+"/",
+
+"/index.html",
+
+"/about.html",
+
+"/pastor.html",
+
+"/worship.html",
+
+"/sermon.html",
+
+"/schedule.html",
+
+"/newfamily.html",
+
+"/css/style.css",
+
+"/images/logo.png"
+
+];
+
+self.addEventListener("install",(event)=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache=>{
+
+return cache.addAll(FILES);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch",(event)=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
